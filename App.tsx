70171786d147fb9807405435eeaff77ff3f4ef17
Nataniel Vasconcelos <nataniel.vasconcelos@tbdc.com.br>
2024-06-19 00:07:39 -0300

    ✨ first commit: said God let there be light

--- App.tsx
+++ App.tsx
@@ -1,118 +1,107 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useState} from 'react';
+import LottieView from 'lottie-react-native';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
+  TextInput,
+  TouchableOpacity,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+export default function App() {
+  const [email, setEmail] = useState<string>('');
+  const [senha, setSenha] = useState<string>('');
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.container}>
+      <View>
+        <LottieView
+          source={require('./src/assets/loginAnimation.json')}
+          autoPlay={true}
+          style={styles.iconContainer}
+        />
+      </View>
+      <View style={styles.welcomeContainer}>
+        <Text style={styles.welcomeTitle}>Bem vindo ao PDV</Text>
+      </View>
+      <View style={styles.loginContainer}>
+        <Text style={styles.labelLogin}>Username ou email</Text>
+        <TextInput
+          placeholder="Digite seu email"
+          value={email}
+          onChangeText={setEmail}
+          style={styles.input}
+        />
+      </View>
+      <View style={styles.loginContainer}>
+        <Text style={styles.labelLogin}>Password</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Digite sua senha"
+          value={senha}
+          onChangeText={setSenha}
+          secureTextEntry={true}
+        />
+      </View>
+      <View style={styles.loginContainer}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  welcomeContainer: {
+    paddingBottom: 50,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  iconContainer: {
+    height: 300,
+    width: 300,
   },
-  highlight: {
-    fontWeight: '700',
+  welcomeTitle: {
+    fontSize: 19,
+    fontWeight: 'bold',
+  },
+  labelLogin: {
+    fontSize: 14,
+    fontWeight: '900',
+  },
+  loginContainer: {
+    width: '100%',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    paddingLeft: 20,
+    paddingBottom: 15,
+  },
+  input: {
+    width: '95%',
+    height: 50,
+    borderColor: '#000',
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    backgroundColor: '#fff',
+    marginTop: 5,
+  },
+  button: {
+    backgroundColor: 'black',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    width: '95%',
+    alignItems: 'center',
+    height: 60,
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 22,
+    textAlign: 'center',
+    paddingTop: 3,
   },
 });
-
-export default App;
